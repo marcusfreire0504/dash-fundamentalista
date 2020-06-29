Fix EBITDA calculation order

funcs.py
@@ -22,12 +22,11 @@ def calc_kpis(df, quarterly=True):
         df.sort_values('DT_FIM_EXERC', inplace=True)
         if quarterly:
             df = add_quarters(df)
-    df['EBITDA'] = df['EBIT'] - df['DepreciationAmortization']
     if 'SEGMENTO' in df.columns:
         df['EBIT'] = np.where(
             df['SEGMENTO'] == 'Bancos', df['EBT'], df['EBIT']
         )
-        df['EBITDA'] = df['EBIT']
+    df['EBITDA'] = df['EBIT'] - df['DepreciationAmortization']
     df['FreeCashFlow'] = df['OperatingCashFlow'] + df['InvestingCashFlow']
     df['Opex'] = df['Revenue'] - df['EBIT']
     df['ShareholderEquity'] = \